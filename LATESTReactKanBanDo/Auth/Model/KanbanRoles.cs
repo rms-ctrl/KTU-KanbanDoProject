1bb6d27096c2fd85242f8dcaa44c060b6f47885f
domas-bootcamp <domas.bootcamp@gmail.com>
2023-11-22 13:17:16 +0200

    Added JWT token based authentication.

---
+++ LATESTReactKanBanDo/Auth/Model/KanbanRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace LATESTReactKanBanDo.Auth.Model
+{
+    public static class KanbanRoles
+    {
+        public const string Admin = nameof(Admin);
+        public const string KanbanUser = nameof(KanbanUser);
+
+        public static readonly IReadOnlyCollection<string> All = new[] {Admin, KanbanUser };
+    }
+}
--- LATESTReactKanBanDo/Migrations/20231115151610_identity.Designer.cs
+++ LATESTReactKanBanDo/Migrations/20231115151610_identity.Designer.cs
@@ -4,6 +4,7 @@ using LATESTReactKanBanDo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LATESTReactKanBanDo.Migrations
 {
     [DbContext(typeof(KanbanDoDbContext))]
-    partial class KanbanDoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231115151610_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
